c596daa8cecc5df8ddc059c6b1166021a6dabec3
myf900 <myf9000@vp.pl>
2015-12-05 18:09:45 +0100

    Fix index in StudentsController, set up root_path

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   resources :students do
     get :subjects
   end
+
+
+  root 'students#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150719115626) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,47 +40,4 @@ ActiveRecord::Schema.define(version: 20150719115626) do
     t.datetime "updated_at",      null: false
   end
 
-  add_index "subject_item_notes", ["student_id"], name: "index_subject_item_notes_on_student_id", using: :btree
-  add_index "subject_item_notes", ["subject_item_id"], name: "index_subject_item_notes_on_subject_item_id", using: :btree
-
-  create_table "subject_items", force: :cascade do |t|
-    t.string   "title"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer  "teacher_id"
-  end
-
-  add_index "subject_items", ["teacher_id"], name: "index_subject_items_on_teacher_id", using: :btree
-
-  create_table "teachers", force: :cascade do |t|
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "academic_title"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.inet     "current_sign_in_ip"
-    t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-  end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-
-  add_foreign_key "participations", "students"
-  add_foreign_key "participations", "subject_items"
-  add_foreign_key "subject_item_notes", "students"
-  add_foreign_key "subject_item_notes", "subject_items"
-  add_foreign_key "subject_items", "teachers"
 end
